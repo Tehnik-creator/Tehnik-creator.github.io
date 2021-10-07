ea9cea6fa53d16196fda0c4b974b5616e393398d
Osipov Maksim <81251231+Tehnik-creator@users.noreply.github.com>
2021-10-07 16:08:32 +0300

    Add files via upload

---
+++ do-not-look-here/README.md
@@ -0,0 +1,5 @@
+# DO-NOT-LOOK-HERE-(EXAM)
+you can't look here
+this is my site i use html, css, cmder, and my arms
+
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
---
+++ do-not-look-here/main.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+    document.getElementById("main_card_btn1").innerHTML="Купленно";
+    }
+
+function myFunction2() {
+    document.getElementById("main_card_btn2").innerHTML="Купленно";
+}
+
+function myFunction3() {
+   document.getElementById("main_card_btn3").innerHTML="Купленно";
+}
