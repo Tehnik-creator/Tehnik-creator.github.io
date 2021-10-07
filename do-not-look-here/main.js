12a98efb4eb9edbb97a826eb065e86275f542408
Alexey Serpovskoy <55462936+SERPOVSKOY@users.noreply.github.com>
2021-10-07 18:46:56 +0300

    Update main.js

--- do-not-look-here/main.js
+++ do-not-look-here/main.js
@@ -1,3 +1,10 @@
+// Вынести онлик и всем манипуляции с элементами производить в этом файле. 
+// const btn1 = document.querySelector('#main_card_btn1');
+// btn1.addEventListener('click', () => {
+//      btn1.innerHTML = '...';
+//});
+
+
 function myFunction1() {
     document.getElementById("main_card_btn1").innerHTML="Купленно";
     }
